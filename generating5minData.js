d9c4deab1d31577c363cfb3ecf68d5518bafc331
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-12-10 11:52:11 +0530

    generating function and other chnages

---
+++ generating5minData.js
@@ -0,0 +1,97 @@
+const fs = require("fs");
+
+const inputFilePath = "data2.csv";
+const outputFilePath = "data3.csv";
+
+const getData = async () => {
+  const resp = fs.readFileSync(inputFilePath, "utf-8");
+
+  const cdata = resp.split("\n").map((row) => {
+    const [dateStart, dateEnd, time, open, high, low, close] = row.split(",");
+
+    if (dateStart && dateEnd && time && open && high && low && close) {
+      let hour, minute;
+
+      if (time.length === 3) {
+        hour = parseInt(time[0]);
+        minute = parseInt(time.slice(1));
+      } else if (time.length === 4) {
+        hour = parseInt(time.slice(0, 2));
+        minute = parseInt(time.slice(2));
+      } else {
+        return null;
+      }
+      const dateTimeStr = `${dateStart.slice(0, 4)}-${dateStart.slice(
+        4,
+        6
+      )}-${dateStart.slice(6)} ${hour.toString().padStart(2, "0")}:${minute
+        .toString()
+        .padStart(2, "0")}:00`;
+
+      const dateTime = new Date(dateTimeStr);
+
+      return {
+        time: dateTime.getTime() / 1000,
+        open: parseFloat(open),
+        high: parseFloat(high),
+        low: parseFloat(low),
+        close: parseFloat(close),
+      };
+    } else {
+      return null;
+    }
+  });
+
+  return cdata.filter((row) => row !== null);
+};
+
+const processData = (data) => {
+  const newData = [];
+
+  for (let i = 0; i < data.length; i += 5) {
+    const chunk = data.slice(i, i + 5);
+
+    if (chunk.length > 0) {
+      const open = chunk[0].open;
+      const close = chunk[chunk.length - 1].close;
+      const high = Math.max(...chunk.map((item) => item.high));
+      const low = Math.min(...chunk.map((item) => item.low));
+      const time = chunk[0].time;
+
+      newData.push({
+        time,
+        open,
+        high,
+        low,
+        close,
+      });
+    }
+  }
+
+  return newData;
+};
+
+const readAndWriteData = async () => {
+  try {
+    const data = await getData();
+    const newData = processData(data);
+
+    // Writing the new CSV file
+    const newCsvContent = newData
+      .map(
+        (item) =>
+          `${item.time},${item.open},${item.high},${item.low},${item.close}`
+      )
+      .join("\n");
+
+    fs.writeFileSync(outputFilePath, newCsvContent, "utf-8");
+
+    console.log(
+      `New CSV file with 5-minute OHLC data has been created: ${outputFilePath}`
+    );
+  } catch (error) {
+    console.error("Error fetching or processing data:", error);
+  }
+};
+
+readAndWriteData();
--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const getData = async () => {
-  const res = await fetch("data2.csv");
+  const res = await fetch("data3.csv");
   const resp = await res.text();
   const cdata = resp.split("\n").map((row) => {
-    const [dateStart, dateEnd, time, open, high, low, close] = row.split(",");
+    const [time, open, high, low, close] = row.split(",");
 
-    if (dateStart && dateEnd && time && open && high && low && close) {
-      let hour, minute;
+    if (time && open && high && low && close) {
+      // let hour, minute;
 
-      if (time.length === 3) {
-        console.log(time);
+      // if (time.length === 3) {
+      //   // console.log(time);
 
-        hour = parseInt(time[0]);
-        minute = parseInt(time.slice(1));
-      } else if (time.length === 4) {
-        hour = parseInt(time.slice(0, 2));
-        minute = parseInt(time.slice(2));
-      } else {
-        return null;
-      }
-      const dateTimeStr = `${dateStart.slice(0, 4)}-${dateStart.slice(
-        4,
-        6
-      )}-${dateStart.slice(6)} ${hour.toString().padStart(2, "0")}:${minute
-        .toString()
-        .padStart(2, "0")}:00`;
+      //   hour = parseInt(time[0]);
+      //   minute = parseInt(time.slice(1));
+      // } else if (time.length === 4) {
+      //   hour = parseInt(time.slice(0, 2));
+      //   minute = parseInt(time.slice(2));
+      // } else {
+      //   return null;
+      // }
+      // const dateTimeStr = `${dateStart.slice(0, 4)}-${dateStart.slice(
+      //   4,
+      //   6
+      // )}-${dateStart.slice(6)} ${hour.toString().padStart(2, "0")}:${minute
+      //   .toString()
+      //   .padStart(2, "0")}:00`;
 
-      const dateTime = new Date(dateTimeStr);
+      // const dateTime = new Date(dateTimeStr);
 
-      //console.log(dateTimeStr);
+      //console.log(dateTimeStr, dateTime);
 
       return {
-        time: dateTime.getTime(),
+        time: time,
         open: parseFloat(open),
         high: parseFloat(high),
         low: parseFloat(low),
@@ -56,11 +56,19 @@ const calculateSMA = (data, period) => {
 
 const displayChart = async () => {
   const chartProperties = {
-    width: 1500,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
     timeScale: {
       timeVisible: true,
       secondsVisible: true,
+    },
+    layout: {
+      background: { color: "#222" },
+      textColor: "#DDD",
+    },
+    grid: {
+      vertLines: { color: "#444" },
+      horzLines: { color: "#444" },
     },
   };
 
@@ -70,74 +78,111 @@ const displayChart = async () => {
   // Fetch and process data
   const klinedata = await getData();
 
+  console.log("data", klinedata);
+
   // Add candlestick series
   const candleseries = chart.addCandlestickSeries();
   candleseries.setData(klinedata);
 
-  // Calculate and add a 10-period SMA line
-  const sma10 = calculateSMA(klinedata, 44);
-  const lineSeries10 = chart.addLineSeries({ color: "blue", lineWidth: 2 });
-  lineSeries10.setData(
-    sma10.map((value, index) => ({ time: klinedata[index + 9].time, value }))
-  );
-
-  // Calculate and add a 44-period high SMA line
-  const sma44High = calculateSMA(klinedata, 44);
-  const lineSeries44High = chart.addLineSeries({
-    color: "green",
-    lineWidth: 2,
+  // Example of applying both properties in a single call
+  chart.timeScale().applyOptions({
+    borderColor: "#71649C",
+    barSpacing: 10,
   });
-  lineSeries44High.setData(
-    sma44High.map((value, index) => ({
-      time: klinedata[index + 43].time,
-      value,
-    }))
-  );
 
-  // Calculate and add a 44-period low SMA line
-  const sma44Low = calculateSMA(klinedata, 44);
-  const lineSeries44Low = chart.addLineSeries({ color: "red", lineWidth: 2 });
-  lineSeries44Low.setData(
-    sma44Low.map((value, index) => ({
-      time: klinedata[index + 43].time,
-      value,
-    }))
-  );
+  // Customizing the Crosshair
+  chart.applyOptions({
+    crosshair: {
+      // Change mode from default 'magnet' to 'normal'.
+      // Allows the crosshair to move freely without snapping to datapoints
+      mode: LightweightCharts.CrosshairMode.Normal,
 
-  let sma10Visible = true;
-  let sma44HighVisible = true;
-  let sma44LowVisible = true;
+      // Vertical crosshair line (showing Date in Label)
+      vertLine: {
+        width: 8,
+        color: "#C3BCDB44",
+        style: LightweightCharts.LineStyle.Solid,
+        labelBackgroundColor: "#9B7DFF",
+      },
 
-  const toggleSMAVisibility = (smaType) => {
-    switch (smaType) {
-      case "sma10":
-        sma10Visible = !sma10Visible;
-        lineSeries10.applyOptions({ visible: sma10Visible });
-        break;
-      case "sma44High":
-        sma44HighVisible = !sma44HighVisible;
-        lineSeries44High.applyOptions({ visible: sma44HighVisible });
-        break;
-      case "sma44Low":
-        sma44LowVisible = !sma44LowVisible;
-        lineSeries44Low.applyOptions({ visible: sma44LowVisible });
-        break;
-      default:
-        break;
-    }
-  };
+      // Horizontal crosshair line (showing Price in Label)
+      horzLine: {
+        color: "#9B7DFF",
+        labelBackgroundColor: "#9B7DFF",
+      },
+    },
+  });
 
-  const addButton = (text, smaType) => {
-    const button = document.createElement("button");
-    button.innerText = text;
-    button.onclick = () => toggleSMAVisibility(smaType);
-    document.body.appendChild(button);
-  };
+  console.log(candleseries);
 
-  // Call the addButton function to create buttons for each SMA indicator
-  addButton("Toggle SMA10", "sma10");
-  addButton("Toggle SMA44 High", "sma44High");
-  addButton("Toggle SMA44 Low", "sma44Low");
+  // Calculate and add a 10-period SMA line
+  // const sma10 = calculateSMA(klinedata, 44);
+  // const lineSeries10 = chart.addLineSeries({ color: "blue", lineWidth: 2 });
+  // lineSeries10.setData(
+  //   sma10.map((value, index) => ({ time: klinedata[index + 9].time, value }))
+  // );
+
+  // // Calculate and add a 44-period high SMA line
+  // const sma44High = calculateSMA(klinedata, 44);
+  // const lineSeries44High = chart.addLineSeries({
+  //   color: "green",
+  //   lineWidth: 2,
+  // });
+  // lineSeries44High.setData(
+  //   sma44High.map((value, index) => ({
+  //     time: klinedata[index + 43].time,
+  //     value,
+  //   }))
+  // );
+
+  // // Calculate and add a 44-period low SMA line
+  // const sma44Low = calculateSMA(klinedata, 44);
+  // const lineSeries44Low = chart.addLineSeries({ color: "red", lineWidth: 2 });
+  // lineSeries44Low.setData(
+  //   sma44Low.map((value, index) => ({
+  //     time: klinedata[index + 43].time,
+  //     value,
+  //   }))
+  // );
+
+  // let sma10Visible = true;
+  // let sma44HighVisible = true;
+  // let sma44LowVisible = true;
+
+  // const toggleSMAVisibility = (smaType) => {
+  //   switch (smaType) {
+  //     case "sma10":
+  //       sma10Visible = !sma10Visible;
+  //       lineSeries10.applyOptions({ visible: sma10Visible });
+  //       break;
+  //     case "sma44High":
+  //       sma44HighVisible = !sma44HighVisible;
+  //       lineSeries44High.applyOptions({ visible: sma44HighVisible });
+  //       break;
+  //     case "sma44Low":
+  //       sma44LowVisible = !sma44LowVisible;
+  //       lineSeries44Low.applyOptions({ visible: sma44LowVisible });
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // };
+
+  // const addButton = (text, smaType) => {
+  //   const button = document.createElement("button");
+  //   button.innerText = text;
+  //   button.onclick = () => toggleSMAVisibility(smaType);
+  //   document.body.appendChild(button);
+  // };
+
+  // // Call the addButton function to create buttons for each SMA indicator
+  // addButton("Toggle SMA10", "sma10");
+  // addButton("Toggle SMA44 High", "sma44High");
+  // addButton("Toggle SMA44 Low", "sma44Low");
+
+  window.addEventListener("resize", () => {
+    chart.resize(window.innerWidth, window.innerHeight);
+  });
 };
 
 displayChart();
